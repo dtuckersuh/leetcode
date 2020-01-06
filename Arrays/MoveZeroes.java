18ceeca67480e3615206ba9055ddf4f463475295
dtuckersuh <suh977@gmail.com>
2020-01-06 12:39:36 -0800

    MoveZeroes problem

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ */
+
+
+public class MoveZeroes{
+    /**
+     * Use two pointers, one to iterate through array and another to track non-zero index
+     */
+    public void moveZeroes(int[] nums){
+        int index = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                // Assign non-zero number to slow pointer
+                nums[index] = nums[i];
+                index++;    // Increment slow pointer
+            }
+        }
+        // Fill rest of array with zeroes
+        for (int i = index; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
--- LinkedLists/IntersectionTwoLists.java
+++ LinkedLists/IntersectionTwoLists.java
@@ -1,3 +1,23 @@
 /**
  * https://leetcode.com/problems/intersection-of-two-linked-lists/
  */
+public class IntersectionTwoLists{
+    /**
+     * Two Pointers Approach:
+     * Maintain two pointers intialized at head, traverse through list.
+     * If pointer reaches end of list, start traverse through other list starting at head
+     * If there is an intersection, the two pointers will meet
+     * Time Complexity: O(m + n) because we traverse through list A and list B
+     * Space Complexity: O(1) because we only use two pointers a and b
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        if (headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
